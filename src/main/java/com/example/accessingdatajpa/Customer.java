711dfb36c874f1e80af4c4b5cf0771a12f42ac42
안승욱 <seunguk@dayoff.ai>
2023-03-30 11:52:02 +0900

    Customer 객체 추가
    - entity로 사용
    - 생성자, getter 추가
    - toString() 메서드 재정의: 고객 id, 성명 출력용도로 사용

---
+++ src/main/java/com/example/accessingdatajpa/Customer.java
@@ -0,0 +1,40 @@
+package com.example.accessingdatajpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("고객[id=%d, 성='%s', 이름='%s']", id, lastName, firstName);
+    }
+}
